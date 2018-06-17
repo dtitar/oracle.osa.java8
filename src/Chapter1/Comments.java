44e5b9bd6cbda700f8fc0752181865fdcc5d1cd8
dtitar <dmitry.titar@gmail.com>
2018-06-17 22:55:04 +0300

    hu

---
+++ src/Chapter1/Comments.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+public class Comments {
+
+    public static void main(String[] args) {
+
+        /*
+         * //must be a space between * and /
+         */
+
+        /* * /* ferret */
+
+    }
+}
